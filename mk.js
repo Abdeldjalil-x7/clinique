1bb93ea9f5dec4633167b08ae1be2449d347b6f0
Abdeldjalil-x7 <abdeldjaliltounsi932@gmail.com>
2024-12-26 11:38:14 +0100

    first commit

---
+++ mk.js
@@ -0,0 +1,10 @@
+ 
+var menu = document.querySelector('.menu i')
+var list = document.querySelector('.home1 .navbar .list ul')
+
+
+menu.addEventListener('click',function(){
+ list.classList.toggle('show')
+});
+
+
